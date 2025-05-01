8586315bab76f3c81ab352ef0a425aa095bb82e7
Maria Eduarda <dudaamadorm@gmail.com>
2025-05-01 17:51:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ package bancoDigital;
 public class Main {
 	public static void main(String[] args) {
 		Banco banco = new Banco();
-        banco.setNome("Banco Digital");
-        System.out.println("Banco: "+banco.getNome()+"\n");
+	        banco.setNome("Banco Digital");
+	        System.out.println("Banco: "+banco.getNome()+"\n");
         
 		Cliente Tuê = new Cliente();
 		Tuê.setNome("Tuê");
@@ -15,7 +15,7 @@ public class Main {
 		Conta cp = new ContaPoupanca(Maria);
 		
 		banco.adicionarConta(cc);
-        banco.adicionarConta(cp);
+        	banco.adicionarConta(cp);
 		
 		cc.depositar(100);
 		cc.transferir(50, cp);	
