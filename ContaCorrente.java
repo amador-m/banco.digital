01f142040df7f4b2e81a444a670e7dfa9c8fd031
Maria Eduarda <dudaamadorm@gmail.com>
2025-05-01 17:47:45 -0300

    Add files via upload

---
+++ ContaCorrente.java
@@ -0,0 +1,16 @@
+package bancoDigital;
+
+public class ContaCorrente extends Conta {	
+	public ContaCorrente(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("\nExtrato conta corrente");
+		imprimirDadosConta();
+	}
+
+	
+
+}
